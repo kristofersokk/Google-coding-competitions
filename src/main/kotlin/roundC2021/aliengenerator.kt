e39c38b175ecdd7060f0ced5c72fdee1987555c1
Kristofer Sokk <krsokk@ttu.ee>
2021-10-30 03:19:36 +0300

    simplify aliengenerator.kt

--- src/main/kotlin/roundC2021/aliengenerator.kt
+++ src/main/kotlin/roundC2021/aliengenerator.kt
@@ -7,13 +7,9 @@ fun main() {
     val cases = readLine()!!.toInt()
     (1..cases).forEach { caseIndex ->
         val G = readLine()!!.toLong()
-        var result = 0L
-        for (i in 1..ceil((2 * G).toDouble().pow(0.5) - 0.5).toLong()) {
+        val result = (1..ceil((2 * G).toDouble().pow(0.5) - 0.5).toLong()).count { i ->
             val numerator = 2 * G - i * i + i
-            if (numerator > 0 && numerator % (2 * i) == 0L) {
-//                println("K: ${upper / (2 * i)}")
-                result += 1
-            }
+            numerator > 0 && numerator % (2 * i) == 0L
         }
         println("Case #$caseIndex: $result")
     }
